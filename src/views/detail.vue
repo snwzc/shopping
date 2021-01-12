686e078aefbe92b3b4bfa9ad2a16594a41047911
魏来 <weilai@1khu.com>
2021-01-12 14:36:21 +0800

    13

--- src/views/detail.vue
+++ src/views/detail.vue
@@ -7,10 +7,7 @@
       @click-left="$router.back()"
     />
 
-    <van-swipe
-      :autoplay="3000"
-      @change="onChange"
-    >
+    <van-swipe :autoplay="3000" @change="onChange">
       <van-swipe-item
         v-for="(image, index) in detailData.detail.imgList"
         :key="index"
@@ -18,13 +15,15 @@
         <img :src="image.smallImg" />
       </van-swipe-item>
       <template #indicator>
-        <div class="custom-indicator">{{ idx + 1 }}/{{detailData.detail.imgList.length}}</div>
+        <div class="custom-indicator">
+          {{ idx + 1 }}/{{ detailData.detail.imgList.length }}
+        </div>
       </template>
     </van-swipe>
     <!-- content  -->
     <div class="content">
       <div class="price-content">
-        <div class="price">￥{{detailData.detail.diffAddressPrice}}</div>
+        <div class="price">￥{{ detailData.detail.diffAddressPrice }}</div>
         <div class="iocn">
           <van-icon name="like-o" />
           收藏
@@ -32,44 +31,41 @@
       </div>
       <div class="markers-text">
         <van-icon name="location-o" />
-        {{detailData.detail.poiInfo.poiName}}
+        {{ detailData.detail.poiInfo.poiName }}
       </div>
       <div class="title">
-        {{detailData.detail.longProductName}}
+        {{ detailData.detail.longProductName }}
       </div>
-
     </div>
     <div>
-      <van-notice-bar
-        :scrollable="false"
-        mode="link"
-        @click="show =true"
-      >
+      <van-notice-bar :scrollable="false" mode="link" @click="show = true">
         <span
           v-for="item in detailData.detail.supplierInfo.policy"
-          :key='item.policyName'
+          :key="item.policyName"
           class="tags"
         >
           <van-icon name="passed" />
-          {{item.policyName}}
+          {{ item.policyName }}
         </span>
       </van-notice-bar>
     </div>
     <!-- richTextInfo -->
-    <div
-      class="text-info"
-      :style="{height:ismore?'auto':'130px'}"
-    >
-      <div
-        class="text-item"
-        v-for="item in detailData.detail.richTextInfo"
-        :key="item.paramName"
-      >
-        <div class="t-left">{{item.paramName}}</div>
-        <div class="t-right">{{item.paramValue}}</div>
+    <div class="text-info">
+      <div class="text-item" v-for="item in info" :key="item.paramName">
+        <div class="t-left">{{ item.paramName }}</div>
+        <div class="t-right">{{ item.paramValue }}</div>
       </div>
-      <div>
-        <van-icon :name="[ismore?'arrow-up':'arrow-down']" />
+      <div
+        @click="showmore"
+        style="
+          text-align: center;
+          padding: 8px 0;
+          font-size: 12px;
+          color: #828282;
+        "
+      >
+        {{ ismore ? "收起" : "展开全部" }}
+        <van-icon :name="ismore ? 'arrow-up' : 'arrow-down'" />
       </div>
     </div>
 
@@ -77,92 +73,128 @@
     <div v-html="detailData.detail.mContent" />
     <!-- footer -->
     <van-goods-action>
-      <van-goods-action-icon
-        icon="chat-o"
-        text="客服"
-      />
-      <van-goods-action-icon
-        icon="shop-o"
-        text="店铺"
-      />
+      <van-goods-action-icon icon="chat-o" text="客服" />
+      <!-- <van-goods-action-icon icon="shop-o" text="店铺" /> -->
+      <van-goods-action-button type="warning" text="加入购物车" />
       <van-goods-action-button
-        color="#be99ff"
-        type="warning"
-        text="加入购物车"
-      />
-      <van-goods-action-button
-        color="#7232dd"
         type="danger"
         text="立即购买"
-        @click="shopping=true"
+        @click="shopping = true"
       />
     </van-goods-action>
 
     <!--  -->
 
-    <van-action-sheet
-      v-model="show"
-      title="服务说明"
-      cancel-text="确认"
-    >
+    <van-action-sheet v-model="show" title="服务说明" cancel-text="确认">
       <div
         class="sheet"
         v-for="item in detailData.detail.supplierInfo.policy"
-        :key='item.policyName'
+        :key="item.policyName"
       >
         <div class="zt">
           <van-icon name="passed" />
-          {{item.policyName}}
+          {{ item.policyName }}
         </div>
         <div class="zmsg">
-          {{item.policyDescription}}
+          {{ item.policyDescription }}
         </div>
       </div>
     </van-action-sheet>
 
     <!--  -->
 
-    <van-sku
-      v-model="shopping"
-      :sku="sku"
-      :goods="goods"
-    />
-
+    <van-sku v-model="shopping" :sku="sku" :goods="goods" />
   </div>
 </template>
 
 <script>
-
+import axios from "axios";
 export default {
-  name: 'Detail',
+  name: "Detail",
   components: {},
   props: {},
-  data () {
+  data() {
     return {
       detailData: {},
       idx: 0,
       show: false,
       shopping: false,
-      goods: { picture: '' },
-      ismore: false
-    }
+      goods: { picture: "" },
+      ismore: true,
+      info: [],
+      name: "",
+      sku: {
+        // 所有sku规格类目与其值的从属关系，比如商品有颜色和尺码两大类规格，颜色下面又有红色和蓝色两个规格值。
+        // 可以理解为一个商品可以有多个规格类目，一个规格类目下可以有多个规格值。
+        tree: [],
+        // 所有 sku 的组合列表，比如红色、M 码为一个 sku 组合，红色、S 码为另一个组合
+        list: [],
+        price: "1.00", // 默认价格（单位元）
+        stock_num: 227, // 商品总库存
+      },
+    };
   },
   watch: {},
   computed: {},
   methods: {
-    onChange (index) {
+    onChange(index) {
       this.idx = index;
     },
-  },
-  created () {
-    let { id } = this.$route.query
-    this.detailData = require('../json/' + id)
-    this.goods.picture = this.detailData.detail.firstDrawImage
-  },
-  mounted () {
+    showmore() {
+      this.ismore = !this.ismore;
+      if (this.ismore) {
+        this.info = this.detailData.detail.richTextInfo;
+      } else {
+        this.info = this.detailData.detail.richTextInfo.slice(0, 4);
+      }
+    },
 
-  }
-}
+    setDetail() {
+      let data = this.detailData.detail.sizeDetail;
+      let tree = [],
+        list = [];
+      data.forEach((item, index) => {
+        let t = {
+          v: [],
+          k_s: "s" + index,
+        };
+        let l = {
+          s1: index,
+          s2: index,
+          price: item.priceOriginStr,
+          stock_num: Math.floor(Math.random() * 10000),
+        };
+        let name = "";
+        item.specs.forEach((item1) => {
+          name += item1.des + " ";
+        });
+        t.v.push({
+          id: index,
+          name,
+          imgUrl: item.smallConfirmImg,
+          previewImgUrl: item.smallConfirmImg,
+        });
+        list.push(l);
+        tree.push(t);
+      });
+
+      this.sku.tree = tree;
+      this.sku.list = list;
+      this.sku.price = list[0].price;
+    },
+  },
+  created() {
+    let { id, name } = this.$route.query;
+    // this.detailData = require("../assets/json/" + name + "/" + id + ".json");
+    axios.get("./json/" + name + "/" + id + ".json").then((res) => {
+      this.detailData = res.data;
+      this.goods.picture = this.detailData.detail.firstDrawImage;
+      this.showmore();
+      this.setDetail();
+    });
+  },
+  mounted() {},
+};
 </script>
 <style  scoped>
 .van-swipe {
@@ -235,8 +267,6 @@ export default {
 }
 .text-info {
   background-color: #eee;
-  min-height: 130px;
-  height: 100px;
 }
 .text-item {
   display: flex;
@@ -245,6 +275,7 @@ export default {
 }
 .t-left {
   min-width: 100px;
+  color: #828282;
 }
 </style>
  
--- src/views/list.vue
+++ src/views/list.vue
@@ -1,77 +1,219 @@
 <template>
   <div class="wrapper">
-
-    <van-row type="flex">
-      <van-col span="5">
-        <van-sidebar
-          class='mun'
-          v-model="activeKey"
-          @change='change'
-        >
-          <van-sidebar-item
-            v-for="item in fldata"
-            :key="item.suId"
-            :title="item.name"
-          />
-        </van-sidebar>
-      </van-col>
-      <van-col span="19">
-
-        <van-card
-          v-for="item in list"
-          :key="item.suId"
-          :desc="item.background"
-          :title="item.title"
-          :thumb="item.image"
-          :price="item.priceStr"
-          @click="detail(item.suId)"
-        />
-
-      </van-col>
-    </van-row>
-
+    <div class="top" :class="[name]">
+      <van-image round width="6rem" height="6rem" :src="headData.head" />
+      <h2>{{ headData.title }}</h2>
+      <p class="p1">{{ headData.msg }}</p>
+      <p class="p2">
+        {{ headData.msg1 }}
+      </p>
+    </div>
+    <div style="height: 230px"></div>
+    <div class="tabs">
+      <van-tabs v-model="active" color="blue">
+        <van-tab title="梦工厂">
+          <van-row type="flex" class="list">
+            <van-col span="5">
+              <van-sidebar
+                class="mun"
+                :style="{ height: height + 'px' }"
+                v-model="activeKey"
+                @change="change"
+                :a="height"
+              >
+                <van-sidebar-item
+                  v-for="item in fldata"
+                  :key="item.suId"
+                  :title="item.name"
+                />
+              </van-sidebar>
+            </van-col>
+            <van-col span="19">
+              <div class="content" :style="{ height: height - 60 + 'px' }">
+                <van-card
+                  v-for="item in list"
+                  :key="item.suId"
+                  :desc="item.background"
+                  :title="item.title"
+                  :thumb="item.image"
+                  :price="item.priceStr"
+                  @click="detail(item.suId)"
+                />
+                <van-divider>到底了...</van-divider>
+              </div>
+            </van-col>
+          </van-row>
+        </van-tab>
+        <van-tab title="动态">
+          <van-divider>暂无动态</van-divider>
+        </van-tab>
+      </van-tabs>
+    </div>
   </div>
 </template>
 
 <script>
-const fldata = require('../json/fenlei')
+import baiyangHead from "../assets/baiyang-head.jpg";
+import xiaopengHead from "../assets/xiaopeng-head.jpg";
+import kankanHead from "../assets/kankan.png";
+import caijingHead from "../assets/caijing-head.jpg";
+import axios from "axios";
 export default {
-  name: 'List',
+  name: "List",
   components: {},
   props: {},
-  data () {
+  data() {
     return {
-      activeKey: '',
-      fldata,
-      list: []
-    }
+      activeKey: "",
+      active: "梦工厂",
+      fldata: [],
+      list: [],
+      headData: {},
+      height: 0,
+      baiyang: {
+        title: "白羊座梦工场",
+        msg: "送给白羊座的定制创意礼物",
+        msg1: "白羊座的人，单纯热烈没心机，做事即使不惊艳，内心也永远不服输。",
+        head: baiyangHead,
+      },
+      xiaopeng: {
+        title: "小鹏汽车梦工场",
+        msg: "无印鹏品",
+        msg1: "小鹏梦工场，精美周边等你发现",
+        head: xiaopengHead,
+      },
+      kankan: {
+        title: "侃侃的梦工厂",
+        msg: "中国内地女歌手",
+        msg1: "欢迎来到我的专属定制梦工厂",
+        head: kankanHead,
+      },
+      caijing: {
+        title: "《财经》 杂志梦工厂",
+        msg: "独立、独家、独到",
+        msg1: "由中国证券市场缔造团队创刊，见证中国金融市场成长。",
+        head: caijingHead,
+      },
+      name: "",
+    };
   },
   watch: {},
   computed: {},
   methods: {
-    change (idx) {
-      let data = require('../json/' + fldata[idx].categoryId)
-      this.list = data
+    change(idx) {
+      // let data = require("../assets/json/" +
+      //   this.name +
+      //   "/" +
+      //   this.fldata[idx].categoryId);
+      axios
+        .get(
+          "./json/" + this.name + "/" + this.fldata[idx].categoryId + ".json"
+        )
+        .then((res) => {
+          this.list = res.data;
+        });
     },
-    detail (id) {
-      console.log(id);
-      this.$router.push({ name: 'Detail', query: { id } })
-    }
+
+    detail(id) {
+      this.$router.push({ name: "Detail", query: { id, name: this.name } });
+    },
   },
-  created () { },
-  mounted () {
-    this.change(1)
-  }
-}
+  created() {
+    let name = "baiyang";
+    switch (this.$route.query.id) {
+      case "1":
+        this.headData = this.baiyang;
+        name = "baiyang";
+        break;
+      case "2":
+        this.headData = this.xiaopeng;
+        name = "xiaopeng";
+        break;
+      case "3":
+        this.headData = this.kankan;
+        name = "kankan";
+        break;
+      case "4":
+        this.headData = this.caijing;
+        name = "caijing";
+        break;
+      default:
+        this.headData = this.baiyang;
+        break;
+    }
+    this.name = name;
+    // this.fldata = require("../assets/json/" + name + "/" + name + ".json");
+    axios.get("./json/" + name + "/" + name + ".json").then((res) => {
+      this.fldata = res.data;
+      this.change(0);
+    });
+    let height = document.documentElement.clientHeight - 250;
+    this.height = height;
+  },
+  mounted() {},
+};
 </script>
 <style  scoped>
+.wrapper {
+  overflow: hidden;
+}
 .mun {
   position: fixed;
   text-align: center;
+  /* height:300px; */
+  overflow-y: auto;
+}
+.content {
+  overflow-y: auto;
 }
 .van-card__price-currency,
 .van-card__price {
   color: red !important;
+}
+.top {
+  width: 100%;
+  text-align: center;
+  padding: 20px 0px;
+  color: #fff;
+  height: 200px;
+  position: fixed;
+  z-index: 9;
+}
+.baiyang {
+  background: url(../assets/baiyang-bg.png) no-repeat 100%;
+}
+.caijing {
+  background: url(../assets/caijing-bg.jpg) no-repeat 100%;
+}
+.xiaopeng {
+  background: url(../assets/xiaopeng-bg.png) no-repeat 100%;
+}
+.kankan {
+  background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+    url(../assets/kankan.png) no-repeat center center;
+  background-size: 60%;
+}
+.tabs {
+  position: fixed;
+  width: 100%;
+  margin-top: 20px;
+}
+h2 {
+  font-size: 18px;
+  margin-top: 5px;
+}
+.p1 {
+  font-size: 12px;
+  color: #66c166;
+  margin-top: 5px;
+}
+.p2 {
+  font-size: 14px;
+  margin-top: 5px;
+}
+.list {
+  overflow: hidden;
+  padding: 10px 0;
 }
 </style>
  
